6d6c65f7df791477f615481f2dac08bc03184554
Harald Husum <harahu@stud.ntnu.no>
2017-05-02 17:41:19 +0200

    more stuff

--- mpso-jssp.py
+++ mpso-jssp.py
@@ -1,22 +1,42 @@
-import random, math
+import random, math, collections
 import numpy as np
+
+Parameters = collections.namedtuple('Parameters',
+                                    ['max_iteration',
+                                     'pop_size',
+                                     'max_omega',
+                                     'min_omega',
+                                     'prob_mie',
+                                     'prob_s',
+                                     'prob_i',
+                                     'prob_inv',
+                                     'tf',
+                                     'beta',
+                                     'c1',
+                                     'c2',
+                                     'v_max'])
+
 
 class Particle(object):
 
-    def __init__(self, problem):
-        self._problem = problem
-        dim = problem.n * problem.m
+    def __init__(self, jssp_problem):
+        self._problem = jssp_problem
+        dim = jssp_problem.n * jssp_problem.m
         self._coordinates = np.array(
             [random.uniform(0, dim) for _ in range(dim)])
         self._velocity = np.array(
             [random.random() for _ in range(dim)])
-        self._solution = Solution(problem, self._coordinates)
+        self._solution = Solution(jssp_problem, self._coordinates)
         self._local_best = self._coordinates[:]
         self._local_best_val = self._solution.makespan
 
     @property
     def coordinates(self):
         return self._coordinates
+
+    @property
+    def solution(self):
+        return self._solution
 
     @property
     def makespan(self):
@@ -28,13 +48,16 @@ class Particle(object):
             self._local_best = self._coordinates[:]
             self._local_best_val = self._solution.makespan
 
-    def update_velocity(self, omega, c1, c2, global_best, v_max):
+    def update_velocity(self, omega, global_best, alg_params):
         self._velocity *= omega
-        self._velocity += c1 * random.random() * (self._local_best - self._coordinates)
-        self._velocity += c2 * random.random() * (global_best - self._coordinates)
+        self._velocity += alg_params.c1 * random.random() * (self._local_best - self._coordinates)
+        self._velocity += alg_params.c2 * random.random() * (global_best - self._coordinates)
+        # Truncate speed for each dimension to v_max
         for i in range(len(self._velocity)):
-            if self._velocity[i] > v_max:
-                self._velocity[i] = v_max
+            if abs(self._velocity[i]) >  alg_params.v_max:
+                if self._velocity[i] < 0:
+                    self._velocity[i] = -alg_params.v_max
+                self._velocity[i] = alg_params.v_max
 
     def move(self):
         self._coordinates += self._velocity
@@ -73,35 +96,36 @@ class Particle(object):
         mutated = mutated[:r] + segment + mutated[r:]
         return mutated
 
-    def _mutate(self, prob_s, prob_i, prob_inv):
+    def _mutate(self, alg_params):
         q = random.random()
-        if 0 <= q <= prob_s:
+        if 0 <= q <= alg_params.prob_s:
             return self._swap()
-        elif prob_s < q <= prob_s + prob_i:
+        elif alg_params.prob_s < q <= alg_params.prob_s + alg_params.prob_i:
             return self._insert()
-        elif prob_s + prob_i < q <= prob_s + prob_i + prob_inv:
+        elif alg_params.prob_s + alg_params.prob_i < q <= alg_params.prob_s + alg_params.prob_i + alg_params.prob_inv:
             return self._inverse()
         else:
             return self._long()
 
-    def enhance(self, t, tf, beta):
-        while t > tf:
-            p_prime_coordinates = #algorithm2
+    def enhance(self, alg_params, global_best_val):
+        t = self.makespan - global_best_val
+        while t > alg_params.tf:
+            p_prime_coordinates = self._mutate(alg_params)
             p_prime = Solution(self._problem, p_prime_coordinates)
             delta = p_prime.makespan - self._solution.makespan
-            if delta < 0 or random.random() < min(1, math.exp(-(delta/T))):
+            if delta < 0 or random.random() < min(1, math.exp(-(delta/t))):
                 self._coordinates = p_prime_coordinates
                 self._refresh()
-            t *= beta
+            t *= alg_params.beta
 
 
 class Solution(object):
 
-    def __init__(self, problem, coordinates):
-        self.problem = problem
+    def __init__(self, jssp_problem, coordinates):
+        self.problem = jssp_problem
         int_series = sorted(range(len(coordinates)),
                             key=lambda i: coordinates[i])
-        operations = [i % problem.n + 1 for i in int_series]
+        operations = [i % jssp_problem.n + 1 for i in int_series]
         self.schedule, self._makespan = self._schedule(operations)
 
     @property
@@ -148,24 +172,43 @@ class Problem(object):
         return self._jobs
 
 
-def mpso(max_iteration, max_omega, min_omega, prob_mie, c1, c2, v_max, problem):
+def mpso(jssp_problem, alg_params):
     # Initialize swarm
-    swarm = [Particle(problem) for _ in range(30)]
+    swarm = [Particle(jssp_problem) for _ in range(alg_params.pop_size)]
     # Initialize global best
     swarm_min = min(swarm, key=lambda x: x.makespan)
     global_best = swarm_min.coordinates[:]
     global_best_val = swarm_min.makespan
-    for i in range(max_iteration):
+    for i in range(alg_params.max_iteration):
+        # Perform local search for particles
         for particle in swarm:
-            if random.random() <= prob_mie:
-                particle.enhance()
+            if random.random() <= alg_params.prob_mie:
+                particle.enhance(alg_params, global_best_val)
         # Update global best
         swarm_min = min(swarm, key=lambda x: x.makespan)
         if swarm_min.makespan < global_best_val:
             global_best = swarm_min.coordinates[:]
             global_best_val = swarm_min.makespan
         # Update omega
-        omega = max_omega - i * (max_omega - min_omega) / max_iteration
+        omega = alg_params.max_omega - i * (alg_params.max_omega - alg_params.min_omega) / alg_params.max_iteration
+        # Move particles
         for particle in swarm:
-            particle.update_velocity(omega, c1, c2, global_best, v_max)
+            particle.update_velocity(omega, global_best, alg_params)
             particle.move()
+    return min(swarm, key=lambda x: x.makespan).solution
+
+
+def read_mpso_problem(filename):
+    pass
+
+if __name__ == '__main__':
+    problem_1 = read_mpso_problem('test_data/1.txt')
+    algorithm_parameters = Parameters(max_iteration=300, pop_size=30,
+                                      max_omega=1.4, min_omega=0.4,
+                                      prob_mie=0.01, prob_s=0.4,
+                                      prob_i=0.4, prob_inv=0.1,
+                                      tf=0.1, beta=0.97,
+                                      c1=2, c2=2,
+                                      v_max=jssp_problem.n * jssp_problem.m * 0.1)
+    solution = mpso(problem_1, algorithm_parameters)
+
